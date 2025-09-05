5805fc53839f58a5f3ae9d7d3b829a7e3ae9d104
sorisk3-git <support@sorisk3.net>
2025-09-05 20:09:05 +0200

    second_message

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def sayHello(request):
+    return HttpResponse('Hello Word!')
+
+def index(request):
+    return render(request, 'index.html', {})
